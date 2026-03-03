dc04e7e5b2f55992887e30d8660a876e951ecaf0
xiao <48696694+xytan0056@users.noreply.github.com>
2026-03-03 13:22:33 -0800

    op work for preparing OSS (#42)
    <!-- Provide a summary in the Title above. Example: "Feature: add user
    authentication" -->
    operational  work for OSS preparation
    
    ## What?
    <!-- What specifically is changing? Provide context for the reviewer.
    -->
    
    - ISSUE template bug report and feature requests
    - CODE_OF_CONDUCT
    - CONTRIBUTING guide
    - liscense header copy from
    https://github.com/uber-go/nilaway/blob/main/LICENSE
    - dependabot
    
    
    ## Test Plan
    <!-- How did you test this? Provide evidence (screenshots, logs, or
    steps to reproduce). -->
    CI 
    ## Issue
    <!-- 
    Link the issue here. 
    - Use 'Closes #123' if this is the final fix. 
    - Use 'Part of #123' or just '#123' if the feature is still in progress.
    -->

--- example/client/client.go
+++ example/client/client.go
@@ -1,7 +1,22 @@
+// Copyright (c) 2025 Uber Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -9,11 +24,11 @@ import (
 	"strings"
 	"time"
 
-	"encoding/json"
-	pb "github.com/uber/tango/tangopb"
 	"go.uber.org/yarpc"
 	yarpcgrpc "go.uber.org/yarpc/transport/grpc"
 	"go.uber.org/zap"
+
+	pb "github.com/uber/tango/tangopb"
 )
 
 func main() {
